cf5bdb3db16fcb5300d76bc048fc0294b4a8c05d
msmaitran <itsmaisn@gmail.com>
2019-09-16 15:00:12 -0700

    EditBookActivity

---
+++ M01/Assignment/ReadingList/app/src/main/java/com/lambdaschool/readinglist/ui/EditBookActivity.kt
@@ -0,0 +1,12 @@
+package com.lambdaschool.readinglist.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditBookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_book)
+    }
+}
