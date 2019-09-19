886d0bc1fc2fcb6190d3e7f615b185e7220a7411
msmaitran <itsmaisn@gmail.com>
2019-09-19 14:52:40 -0700

    Build the Room database.

--- M04/Assignment/ReadingList/app/src/main/java/com/lambdaschool/readinglist/database/BookDBRepo.kt
+++ M04/Assignment/ReadingList/app/src/main/java/com/lambdaschool/readinglist/database/BookDBRepo.kt
@@ -2,24 +2,33 @@ package com.lambdaschool.readinglist.database
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.room.Room
 import com.lambdaschool.readinglist.BookRepoInterface
 import com.lambdaschool.readinglist.model.Book
 
 // Create the Database repo and implement the methods
 class BookDBRepo(val context: Context) : BookRepoInterface {
     override fun createEntry(entry: Book) {
-
+        database.bookDao().createEntry(entry)
     }
 
     override fun readAllEntries(): LiveData<List<Book>> {
-        return
+        return database.bookDao().readAllEntries()
     }
 
     override fun updateEntry(entry: Book) {
-
+        database.bookDao().updateEntry(entry)
     }
 
     override fun deleteEntry(entry: Book) {
+        database.bookDao().deleteEntry(entry)
+    }
 
+    // Build the Room database
+    private val database by lazy {
+        Room.databaseBuilder(
+            context.applicationContext,
+            BookDB::class.java, "entry_database"
+        ).fallbackToDestructiveMigration().build()
     }
 }
